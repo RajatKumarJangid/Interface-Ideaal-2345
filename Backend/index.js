4086a5b4491a5da18f3bbd9fffdc45352d21d32c
Rajat Kumar Jangid <jangidrajat105@gmail.com>
2024-03-30 11:48:34 +0530

    login, register and logout functionality added

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const {connection} = require("./config/db");
+const {userRouter} = require("./routes/user.routes");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRouter)
+
+
+app.get('/',(req,res) =>{
+    res.send({
+        msg:"This is the home route"
+    })
+})
+
+
+app.listen(process.env.PORT, async()=>{
+    try {
+        await connection;
+        console.log(`Connected with Database`);
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    } catch (error) {
+        console.log(`Error occured : ${error}`);
+    }
+})
---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const {BlackTokenModel} =require("../models/blacklist.model");
+require("dotenv").config();
+
+const auth =async(req,res,next) =>{
+
+    try{
+        const token =req.headers.authorization?.split(' ')[1];
+        if(!token){
+            return res.status(404).send({
+                msg:"please login"
+            })
+        }
+         const blacktoken =await BlackTokenModel.findOne({blackToken:token})
+        if(!blacktoken){
+            const decoded =jwt.verify(token,process.env.tokenSecretKey)
+            if(decoded){
+                console.log(decoded)
+                req.body.userId = decoded.userId
+                req.body.author = decoded.author
+    
+                next()
+            }else{
+                res.status(404).send({
+                    msg:'you are not authorized'
+                })
+            }
+        }else{
+            res.status(404).send({
+                msg:'you are not authorized'
+            })
+        }
+
+    }catch(err){
+        console.log(err)
+        res.status(404).send({
+            msg:"error in middleware"
+        })
+    }
+}
+
+
+module.exports ={
+    auth
+}
