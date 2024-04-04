8990c8d792e0436a2b9a8b29fec18609df1451b1
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2024-04-04 10:48:33 +0530

    note

--- client/register/register.js
+++ client/register/register.js
@@ -1,15 +1,121 @@
+const url = "http://localhost:4500";
+
 const signInBtn = document.getElementById("signIn");
-const signUpBtn = document.getElementById("signUp");
+const signIn = document.getElementById("signInBtn");
+const signInPass = document.getElementById("signInPass");
+const signInEmail = document.getElementById("signInEmail");
+const signUp = document.getElementById("signUp");
+const signUpBtn = document.getElementById("signUpBtn");
+const signUpUserName = document.getElementById("signUpUserName");
+const signUpPassword = document.getElementById("signUpPassword");
+const signUpEmail = document.getElementById("signUpEmail");
 const fistForm = document.getElementById("form1");
 const secondForm = document.getElementById("form2");
 const container = document.querySelector(".container");
 
 signInBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
+  container.classList.remove("right-panel-active");
 });
 
-signUpBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
+signUp.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+function showMessage(wrapper, message, color) {
+  const messageContainer = document.createElement("p");
+  messageContainer.textContent = message;
+  messageContainer.style.color = color;
+
+  wrapper.innerHTML = "";
+
+  wrapper.appendChild(messageContainer);
+}
+
+signUpBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const userName = signUpUserName.value;
+  const email = signUpEmail.value;
+  const pass = signUpPassword.value;
+  //   console.log(user, email, pass);
+
+  if (!userName || !pass || !email) {
+    console.log("fill all fields");
+    showMessage(
+      signUpMessageWrapper,
+      "Please fill in all required fields.",
+      "red"
+    );
+    return;
+  }
+
+  const reqData = {
+    userName,
+    email,
+    pass,
+  };
+
+  try {
+    const res = await fetch(`${url}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqData),
+    });
+
+    if (res.ok) {
+      showMessage(signUpMessageWrapper, data.message, "green");
+      const data = await res.json();
+    } else {
+      const err = await res.json();
+      showMessage(
+        signUpMessageWrapper,
+        err.message || "An error occurred.",
+        "red"
+      );
+    }
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    showMessage(
+      signUpMessageWrapper,
+      "An error occurred. Please try again later.",
+      "red"
+    );
+  }
+});
+
+signIn.addEventListener("click", async (e) => {
+  const email = signInEmail.value;
+  const pass = signInPass.value;
+
+  const reqData = {
+    email,
+    pass,
+  };
+
+  try {
+    const res = await fetch(`${url}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqData),
+    });
+    if (res.ok) {
+      showMessage(signUpMessageWrapper, data.message, "green");
+      const data = await res.json();
+    } else {
+      const err = await res.json();
+      showMessage(
+        signUpMessageWrapper,
+        err.message || "An error occurred.",
+        "red"
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 fistForm.addEventListener("submit", (e) => e.preventDefault());
--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const socket = require("socket.io");
+
 const app = express();
+app.use(cors);
 
-app.use(express.json());
+const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.json("this is home page");
-});
-
-app.listen(4500, () => {
+server.listen(4500, () => {
   console.log("server is running on port 4500");
 });
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("send-changes", (delta) => {
+    // console.log(delta);
+    socket.broadcast.emit("receive-changes", delta);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+
